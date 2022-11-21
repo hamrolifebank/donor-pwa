9fbe8bab5f123fc3d6950590906372c52f4c122f
Serena Black <sunsetsin1.sy@gmail.com>
2022-11-21 18:01:39 +0545

    feat: home page start

--- src/sections/home/Home.js
+++ src/sections/home/Home.js
@@ -1,7 +1,24 @@
 import React from "react";
+import { useAppAuthContext } from "@contexts/AuthContext";
+import { Button, Box, Typography, Paper } from "@mui/material";
+
+import { Container } from "@mui/system";
 
 const Home = () => {
-  return <div>Home page</div>;
+  const { publicAddress, user } = useAppAuthContext();
+  return (
+    <Container>
+      <Box display="flex" justifyContent="center">
+        <Typography variant="subtitle3" sx={{ pb: 2 }}>
+          {user.fullname}
+        </Typography>
+      </Box>
+      <Box display="flex" justifyContent="center">
+        <Typography variant="subtitle3" sx={{ pb: 2 }}>
+          {publicAddress}
+        </Typography>
+      </Box>
+    </Container>
+  );
 };
-
 export default Home;
