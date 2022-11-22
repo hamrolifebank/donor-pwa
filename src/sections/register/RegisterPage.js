674b991340647df330554df48d7045534e8b4153
rajutamanglama63 <rajutamanglama63@gmail.com>
2022-11-22 11:11:22 +0545

    refactor: changed newly added props to events

--- src/sections/register/RegisterPage.js
+++ src/sections/register/RegisterPage.js
@@ -30,11 +30,7 @@ export default function Register() {
     gender: "Choose gender",
     bloodGroup: "Choose group",
     dob: "",
-    status: {
-      isRegistered: false,
-      isDonated: false,
-      isVerified: false,
-    },
+    events: [],
   });
 
   const { addUser, deleteWallet } = useAppAuthContext();
