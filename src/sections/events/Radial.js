8f207220e003f7802a1b15b9065943eb199e6913
Amir thapa magar <amirthapa2019@gmail.com>
2022-11-22 21:30:27 +0545

    feat:event page detailed

---
+++ src/sections/events/Radial.js
@@ -0,0 +1,99 @@
+import React, { useState } from "react";
+import ReactApexChart from "react-apexcharts";
+
+const Radial = () => {
+  const [options, setOptions] = useState({
+    chart: {
+      height: 350,
+      type: "radialBar",
+      toolbar: {
+        show: true,
+      },
+    },
+    plotOptions: {
+      radialBar: {
+        startAngle: -135,
+        endAngle: 225,
+        hollow: {
+          margin: 0,
+          size: "70%",
+          background: "#fff",
+          image: undefined,
+          imageOffsetX: 0,
+          imageOffsetY: 0,
+          position: "front",
+          dropShadow: {
+            enabled: true,
+            top: 3,
+            left: 0,
+            blur: 4,
+            opacity: 0.24,
+          },
+        },
+        track: {
+          background: "#fff",
+          strokeWidth: "67%",
+          margin: 0, // margin is in pixels
+          dropShadow: {
+            enabled: true,
+            top: -3,
+            left: 0,
+            blur: 4,
+            opacity: 0.35,
+          },
+        },
+
+        dataLabels: {
+          show: true,
+          name: {
+            offsetY: -10,
+            show: true,
+            color: "#888",
+            fontSize: "17px",
+          },
+          value: {
+            formatter: function (val) {
+              return parseInt(val);
+            },
+            color: "#111",
+            fontSize: "36px",
+            show: true,
+          },
+        },
+      },
+    },
+    fill: {
+      type: "gradient",
+      gradient: {
+        shade: "dark",
+        type: "horizontal",
+        shadeIntensity: 0.5,
+        gradientToColors: ["#00FF00"],
+        inverseColors: true,
+        opacityFrom: 1,
+        opacityTo: 1,
+        stops: [0, 100],
+      },
+    },
+    stroke: {
+      lineCap: "round",
+    },
+    labels: ["Pints"],
+  });
+  const [series, setSeries] = useState([75]);
+  const [labels, setLabels] = useState("Pints");
+  return (
+    <div className="mixed-chart">
+      {typeof window !== "undefined" && (
+        <ReactApexChart
+          options={options}
+          series={series}
+          type="radialBar"
+          width={500}
+        />
+      )}
+    </div>
+  );
+};
+
+export default Radial;
