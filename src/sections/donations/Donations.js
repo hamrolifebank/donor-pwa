f8715d3c6f6749a67120e5bf431a2b594f9b60b2
rajutamanglama63 <rajutamanglama63@gmail.com>
2022-11-22 12:47:23 +0545

    feat: registered event displaying logic configured

--- src/sections/donations/Donations.js
+++ src/sections/donations/Donations.js
@@ -1,7 +1,29 @@
+import { useAppAuthContext } from "@contexts/AuthContext";
 import React from "react";
 
 const Donations = () => {
-  return <div>This is Donations page.</div>;
+  const { user } = useAppAuthContext();
+  const { events } = user;
+
+  // const registeredEvents = () => {
+  //   return events.map((event) => (event.isRegistered ? event : null));
+  // };
+
+  // console.log(registeredEvents());
+  return (
+    <>
+      {events.map((event) => (
+        <>
+          <div key={event.id}>
+            <div>{event.name}</div>
+            <div>{event.date}</div>
+            <div>{event.location}</div>
+          </div>
+          <br />
+        </>
+      ))}
+    </>
+  );
 };
 
 export default Donations;
