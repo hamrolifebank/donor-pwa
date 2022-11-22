ec735ba2e1ebbc9c1c2d509ae3d15d8f5166d485
pitamberrana <pitamber11rana@gmail.com>
2022-11-22 15:54:21 +0545

    seperate radial bar component

--- src/sections/donations/Donations.js
+++ src/sections/donations/Donations.js
@@ -2,45 +2,63 @@ import React from "react";
 import { Box } from "@mui/system";
 import { useAppAuthContext } from "@contexts/AuthContext";
 import {
-  CircularProgressbar,
-  CircularProgressbarWithChildren,
-  buildStyles,
-} from "react-circular-progressbar";
-import "react-circular-progressbar/dist/styles.css";
+  Breadcrumbs,
+  Container,
+  Grid,
+  Link,
+  Paper,
+  Typography,
+} from "@mui/material";
+import { PrimaryButton } from "@components/Button";
+import { useTheme } from "@emotion/react";
+import { Icon } from "@iconify/react";
+import RadialChart from "./RadialChart";
 
-const percentage = 90;
 const Donations = () => {
+  const theme = useTheme();
   const { user } = useAppAuthContext();
   const { events } = user;
-  console.log("hello", events);
+
   return (
-    <>
-      <Box width={100} height={100}>
-        <CircularProgressbarWithChildren
-          value={75}
-          styles={buildStyles({
-            pathColor: "#f00",
-            trailColor: "#000000",
-          })}
-        >
-          <div style={{ width: "84%" }}>
-            <CircularProgressbar value={60} text={`${percentage}`} />
-          </div>
-        </CircularProgressbarWithChildren>
-      </Box>
-      <div>
+    <Container>
+      <Grid container item xs={12} gap={6} margin={1}>
+        <Grid item xs={6}>
+          <Typography variant="h3" sx={{ mb: 2 }}>
+            Pints Donated
+          </Typography>
+          <PrimaryButton> Add donation </PrimaryButton>
+        </Grid>
+        <Grid item xs={3}>
+          <RadialChart />
+        </Grid>
+      </Grid>
+
+      <hr style={{ border: "0.5px dashed black" }} />
+      <Breadcrumbs display="flex" gap={2} justify-content="center">
+        <Link underline="hover">
+          <Icon icon="material-symbols:event-note" />
+          My events{" "}
+        </Link>
+        <Link underline="hover">
+          <Typography>
+            <Icon icon="material-symbols:event-available" />
+            Past events
+          </Typography>
+        </Link>
+      </Breadcrumbs>
+      <Box>
         {events.map((event) => (
-          <>
-            <div key={event.id}>
-              <div>{event.name}</div>
-              <div>{event.date}</div>
-              <div>{event.location}</div>
-            </div>
-            <br />
-          </>
+          <Paper
+            key={event.id}
+            sx={{ m: 1, p: 2, background: theme.palette.grey[300] }}
+          >
+            <div>{event.name}</div>
+            <div>{event.date}</div>
+            <div>{event.location}</div>
+          </Paper>
         ))}
-      </div>
-    </>
+      </Box>
+    </Container>
   );
 };
 
