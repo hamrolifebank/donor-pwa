a3680a2f8d8c702aedd6987a65f27c78f7653556
pitamberrana <pitamber11rana@gmail.com>
2022-11-25 10:02:29 +0545

    unknown commit

--- src/sections/donations/Donations.js
+++ src/sections/donations/Donations.js
@@ -42,7 +42,7 @@ const Donations = () => {
     push(PATH_ADDDONATION.addDonations);
   };
 
-  // var verifiedClr = event.isVerified  ? "blue" : "grey";
+  const currDate = new Date();
 
   return (
     <Container>
@@ -84,12 +84,14 @@ const Donations = () => {
           <TabPanel value="1" sx={{ p: "10px" }}>
             {events?.slice(0, next)?.map((event) =>
               event.isRegistered ? (
-                <Link
-                  href={`/events/${event.id}`}
-                  style={{ textDecoration: "none" }}
-                >
-                  <EventCardRegistered event={event} />
-                </Link>
+                <div key={event.id}>
+                  <Link
+                    href={`/events/${event.id}`}
+                    style={{ textDecoration: "none" }}
+                  >
+                    <EventCardRegistered event={event} />
+                  </Link>
+                </div>
               ) : null
             )}
             <Box display="flex" justifyContent="center">
@@ -100,66 +102,70 @@ const Donations = () => {
           </TabPanel>
           <TabPanel value="2" sx={{ p: "10px" }}>
             {events?.slice(0, next)?.map((event) =>
-              event.isVerified ? (
-                <Link
-                  href={`/events/${event.id}`}
-                  style={{ textDecoration: "none" }}
-                >
-                  <Paper
-                    sx={{
-                      mb: 2,
-                      p: "10px 0px 10px 10px",
-                      backgroundColor: "grey.200",
-                    }}
+              new Date(event.date) < currDate ? (
+                <div key={event.id}>
+                  <Link
+                    href={`/events/${event.id}`}
+                    style={{ textDecoration: "none" }}
                   >
-                    <Grid container item xs={12} gap={1}>
-                      <Grid item xs={1}>
-                        <Icon
-                          icon="material-symbols:verified"
-                          color="blue"
-                          height="22px"
-                        />
-                      </Grid>
-                      <Grid item xs={8}>
-                        <Typography variant="subtitle1">
-                          {event.name}
-                        </Typography>
+                    <Paper
+                      sx={{
+                        mb: 2,
+                        p: "10px 0px 10px 10px",
+                        backgroundColor: "grey.200",
+                      }}
+                    >
+                      <Grid container item xs={12} gap={1}>
+                        <Grid item xs={1}>
+                          <Icon
+                            icon="material-symbols:verified"
+                            color={event.isVerified ? "blue" : "grey"}
+                            height="22px"
+                          />
+                        </Grid>
+                        <Grid item xs={8}>
+                          <Typography variant="subtitle1">
+                            {event.name}
+                          </Typography>
 
-                        <Typography
-                          sx={{
-                            fontSize: "subtitle2.fontSize",
-                            color: "grey.600",
-                            display: "flex",
-                            alignItems: "center",
-                            gap: 1,
-                          }}
-                        >
-                          <Icon icon="mdi:clock-time-eight-outline" />
-                          {event.date.slice(0, 10)}
-                        </Typography>
+                          <Typography
+                            sx={{
+                              fontSize: "subtitle2.fontSize",
+                              color: "grey.600",
+                              display: "flex",
+                              alignItems: "center",
+                              gap: 1,
+                            }}
+                          >
+                            <Icon icon="mdi:clock-time-eight-outline" />
+                            {event.date.slice(0, 10)}
+                          </Typography>
 
-                        <Typography
-                          sx={{
-                            fontSize: "subtitle2.fontSize",
-                            display: "flex",
-                            gap: 1,
-                            alignItems: "center",
-                            textDecoration: "underline",
-                            color: "primary.main",
-                          }}
-                        >
-                          <Icon icon="material-symbols:location-on" />
-                          {event.location}
-                        </Typography>
+                          <Typography
+                            sx={{
+                              fontSize: "subtitle2.fontSize",
+                              display: "flex",
+                              gap: 1,
+                              alignItems: "center",
+                              textDecoration: "underline",
+                              color: "primary.main",
+                            }}
+                          >
+                            <Icon icon="material-symbols:location-on" />
+                            {event.location}
+                          </Typography>
+                        </Grid>
+                        <Grid item xs={1}>
+                          {event.manuallyAdded ? (
+                            <IconButton sx={{ height: 10 }}>
+                              <CancelRoundedIcon />
+                            </IconButton>
+                          ) : null}
+                        </Grid>
                       </Grid>
-                      <Grid xs={1}>
-                        <IconButton sx={{ height: 10 }}>
-                          <CancelRoundedIcon />
-                        </IconButton>
-                      </Grid>
-                    </Grid>
-                  </Paper>
-                </Link>
+                    </Paper>
+                  </Link>
+                </div>
               ) : null
             )}
             <Box display="flex" justifyContent="center">
