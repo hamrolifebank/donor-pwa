14bbc6972308eec42d759cde7d3c4b6ec368f1d9
rajutamanglama63 <rajutamanglama63@gmail.com>
2022-11-29 15:16:26 +0545

    feat: test api route created for otp verification

---
+++ src/pages/api/otp/otpVerification/index.js
@@ -0,0 +1,21 @@
+import { otpList } from "src/otpList/otpList";
+
+export default function handler(req, res) {
+  if (req.method === "POST") {
+    const { otpProvidedByUser } = req.body;
+    if (otpProvidedByUser) {
+      const matchedOtp = otpList.find((otp) => otp === otpProvidedByUser);
+      const tries = [];
+      if (!matchedOtp && tries.length < 2) {
+        tries.push("try");
+        res
+          .status(400)
+          .json({ msg: "You entered wrong otp, you have 1 more tries" });
+      } else if (!matchedOtp && tries.length === 2) {
+        res.status(400).json({ msg: "Please contact our team" });
+      } else {
+        res.status(200).json({ msg: "Phone number successfully verified" });
+      }
+    }
+  }
+}
