8de23dd65f7eb72ca929cf4c728c27442e9759d6
pitamberrana <pitamber11rana@gmail.com>
2022-11-29 17:03:10 +0545

    feat: clicking a box outside trigger register botton issue fixed

--- src/components/event-card/EventCardNotRegistered.js
+++ src/components/event-card/EventCardNotRegistered.js
@@ -1,6 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types";
-import { Box, Button, Paper, Typography } from "@mui/material";
+import { Box, Button, Grid, Paper, Typography } from "@mui/material";
 import { useTheme } from "@emotion/react";
 import { Icon } from "@iconify/react";
 import { PrimaryButton } from "@components/Button";
@@ -9,74 +9,67 @@ import Link from "next/link";
 
 const EventCardNotRegistered = ({ event }) => {
   const theme = useTheme();
-  const { user, addUser,addEventInUser } = useAppAuthContext();
+  const { user, addUser, addEventInUser } = useAppAuthContext();
 
   const handleRegister = (event) => {
-   addEventInUser(event)
+    addEventInUser(event);
   };
   return (
     <>
       <Paper
         sx={{
-          display: "flex",
           mb: 2,
-          justifyContent: "space-between",
-
-          padding: "12px 20px 12px 12px",
-
+          p: "10px ",
           backgroundColor: "grey.200",
         }}
       >
-        <Box>
-          <Typography
-            sx={{
-              color: "black",
-              fontSize: "subtitle1.fontSize",
-              fontWeight: "subtitle1.fontWeight",
-              lineHeight: "subtitle1.lineHeight",
-            }}
-          >
-            {event.name}
-          </Typography>
-          <Typography
-            sx={{
-              fontSize: "subtitle2.fontSize",
-              color: "grey.600",
-              display: "flex",
-              alignItems: "center",
-              gap: "5px",
-            }}
-          >
-            <Icon icon="mdi:clock-time-eight-outline" />
-            {event.date.slice(0, 10)}
-          </Typography>
+        <Grid container item xs={12}>
+          <Grid item xs={8}>
+            <Typography variant="subtitle1" color="black">
+              {event.name}
+            </Typography>
+          </Grid>
 
-          <Typography
-            sx={{
-              fontSize: "subtitle2.fontSize",
-              display: "flex",
-              gap: "5px",
-              alignItems: "center",
-              textDecoration: "underline",
-              color: "primary.main",
-            }}
-          >
-            <Icon icon="material-symbols:location-on" />
-            {event.location}
-          </Typography>
-        </Box>
-        <Box>
-          <PrimaryButton
-            onClick={(e) => {
-              e.preventDefault();
-              e.stopPropagation();
-              handleRegister(event);
-            }}
-          >
-            <Icon icon="material-symbols:arrow-circle-left" />
-            Register
-          </PrimaryButton>
-        </Box>
+          <Grid item xs={4}>
+            <PrimaryButton
+              onClick={(e) => {
+                e.preventDefault();
+                e.stopPropagation();
+                handleRegister(event);
+              }}
+              sx={{ height: 35 }}
+            >
+              <Icon icon="material-symbols:arrow-circle-left" />
+              Register
+            </PrimaryButton>
+          </Grid>
+        </Grid>
+        <Typography
+          sx={{
+            fontSize: "subtitle2.fontSize",
+            color: "grey.600",
+            display: "flex",
+            alignItems: "center",
+            gap: "5px",
+          }}
+        >
+          <Icon icon="mdi:clock-time-eight-outline" />
+          {event.date.slice(0, 10)}
+        </Typography>
+
+        <Typography
+          sx={{
+            fontSize: "subtitle2.fontSize",
+            display: "flex",
+            gap: "5px",
+            alignItems: "center",
+            textDecoration: "underline",
+            color: "primary.main",
+          }}
+        >
+          <Icon icon="material-symbols:location-on" />
+          {event.location}
+        </Typography>
       </Paper>
     </>
   );
--- src/components/event-card/EventCardRegistered.js
+++ src/components/event-card/EventCardRegistered.js
@@ -1,6 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types";
-import { Box, Button, Paper, Typography } from "@mui/material";
+import { Box, Button, Grid, Paper, Typography } from "@mui/material";
 import { useTheme } from "@emotion/react";
 import { Icon } from "@iconify/react";
 import { PrimaryButton } from "@components/Button";
@@ -12,59 +12,51 @@ const EventCardRegistered = ({ event }) => {
     <>
       <Paper
         sx={{
-          display: "flex",
           mb: 2,
-          justifyContent: "space-between",
-
-          padding: "12px 20px 12px 12px",
-
+          p: "10px ",
           backgroundColor: "grey.200",
         }}
       >
-        <Box>
-          <Typography
-            sx={{
-              color: "black",
-              fontSize: "subtitle1.fontSize",
-              fontWeight: "subtitle1.fontWeight",
-              lineHeight: "subtitle1.lineHeight",
-            }}
-          >
-            {event.name}
-          </Typography>
-          <Typography
-            sx={{
-              fontSize: "subtitle2.fontSize",
-              color: "grey.600",
-              display: "flex",
-              alignItems: "center",
-              gap: "5px",
-            }}
-          >
-            <Icon icon="mdi:clock-time-eight-outline" />
-            {event.date.slice(0, 10)}
-          </Typography>
+        <Grid container item xs={12}>
+          <Grid item xs={7}>
+            <Typography variant="subtitle1" color="black">
+              {event.name}
+            </Typography>
+          </Grid>
+          <Grid item xs={5}>
+            <PrimaryButton disabled sx={{ height: 35 }}>
+              <Icon icon="material-symbols:arrow-circle-left" />
+              Registered
+            </PrimaryButton>
+          </Grid>
+        </Grid>
 
-          <Typography
-            sx={{
-              fontSize: "subtitle2.fontSize",
-              display: "flex",
-              gap: "5px",
-              alignItems: "center",
-              textDecoration: "underline",
-              color: "primary.main",
-            }}
-          >
-            <Icon icon="material-symbols:location-on" />
-            {event.location}
-          </Typography>
-        </Box>
-        <Box>
-          <PrimaryButton disabled>
-            <Icon icon="material-symbols:arrow-circle-left" />
-            Registered
-          </PrimaryButton>
-        </Box>
+        <Typography
+          sx={{
+            fontSize: "subtitle2.fontSize",
+            color: "grey.600",
+            display: "flex",
+            alignItems: "center",
+            gap: "5px",
+          }}
+        >
+          <Icon icon="mdi:clock-time-eight-outline" />
+          {event.date.slice(0, 10)}
+        </Typography>
+
+        <Typography
+          sx={{
+            fontSize: "subtitle2.fontSize",
+            display: "flex",
+            gap: "5px",
+            alignItems: "center",
+            textDecoration: "underline",
+            color: "primary.main",
+          }}
+        >
+          <Icon icon="material-symbols:location-on" />
+          {event.location}
+        </Typography>
       </Paper>
     </>
   );
