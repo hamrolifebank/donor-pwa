abeb6355497a6d773dd626b20c8799f99a589a5a
rajutamanglama63 <rajutamanglama63@gmail.com>
2022-12-07 10:40:40 +0545

    feat: otp testing done on mailinator

--- src/pages/api/otp/requestOtp/index.js
+++ src/pages/api/otp/requestOtp/index.js
@@ -1,11 +1,34 @@
-export default function handler(req, res) {
+const ethers = require("ethers");
+const axios = require("axios");
+
+const handler = async (req, res) => {
   if (req.method === "POST") {
     const phoneNumber = req.body.phoneNum;
 
     if (phoneNumber) {
-      res.status(200);
+      let wallet = new ethers.Wallet(
+        "0x8fb2d8e49fdc7bb4cc946e41813f335024eb766300cada35f80545341d19273f"
+      );
+      console.log("Wallet address:", wallet.address);
+
+      const signature = await wallet.signMessage("rumsan");
+
+      const otp = Math.floor(1000 + Math.random() * 9000);
+
+      const { data } = await axios.post(
+        `https://services.rumsan.net/sms`,
+        { phone: phoneNumber, message: JSON.stringify(otp) },
+        {
+          headers: {
+            signature,
+          },
+        }
+      );
+      console.log(data);
     } else {
       res.status(400).json({ msg: "Please specificy your phone number" });
     }
   }
-}
+};
+
+export default handler;
