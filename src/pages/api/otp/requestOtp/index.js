324fce6d21491d348d967d3d5b8d745df1dc0319
Amir thapa magar <amirthapa2019@gmail.com>
2022-12-09 17:32:52 +0545

    feat: removed console

--- src/pages/api/otp/requestOtp/index.js
+++ src/pages/api/otp/requestOtp/index.js
@@ -14,31 +14,26 @@ export default withIronSessionApiRoute(otpRoute, {
 async function otpRoute(req, res) {
   if (req.method === "POST") {
     const phoneNumber = req.body.phoneNum;
-    console.log("the req session is", req.session);
     if (phoneNumber) {
       let wallet = new ethers.Wallet(
         "0x8fb2d8e49fdc7bb4cc946e41813f335024eb766300cada35f80545341d19273f"
       );
-      console.log("Wallet address:", wallet.address);
 
       const signature = await wallet.signMessage("rumsan");
 
       const otp = Math.floor(1000 + Math.random() * 9000);
 
       if (Object.keys(req.session).length) {
-        console.log("the if entered");
         req.session = {
           ...req.session,
           otp,
         };
       } else {
-        console.log("the else entered");
         req.session = {
           otp,
           tries: 5,
         };
       }
-      console.log("the line 40 ", req.session);
       await req.session.save();
       res.send({ ok: true });
 
@@ -51,7 +46,6 @@ async function otpRoute(req, res) {
           },
         }
       );
-      console.log(data);
     } else {
       res.status(400).json({ msg: "Please specificy your phone number" });
     }
